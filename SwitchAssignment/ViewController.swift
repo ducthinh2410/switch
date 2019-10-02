69229c32bc4ec9482795f02846de005366470a71
Thinh Vo <ducthinh2410@gmail.com>
2019-10-02 10:27:55 +0300

    Initial Commit

---
+++ SwitchAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwitchAssignment
+//
+//  Created by Thinh Vo on 2.10.2019.
+//  Copyright © 2019 Thinh Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
