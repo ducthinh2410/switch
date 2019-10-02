f5e5eb77ccdbf875d7b4e8327b01cfdad7271888
Thinh Vo <ducthinh2410@gmail.com>
2019-10-02 10:49:19 +0300

    Initialized pod. Removed Scene

--- SwitchAssignment/ViewController.swift
+++ SwitchAssignment/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySwitch: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
